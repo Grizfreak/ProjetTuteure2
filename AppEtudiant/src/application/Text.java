193746ce59c09d12e5abf9139eed3abe465cddc0
Grizfreak <36886471+Grizfreak@users.noreply.github.com>
2021-05-11 09:30:02 +0200

    Translation commited
    New in this version :
    - Word comprehension and translation works
    Later :
    Dark Mode

--- AppEtudiant/src/application/Text.java
+++ AppEtudiant/src/application/Text.java
@@ -1,15 +1,37 @@
 package application;
 
+import java.util.HashMap;
+
 public class Text {
-		public String text;
-		public String textCache="";
-		public char occultation='#';
+		private String text;
+		private String textCache="";
+		private char occultation='#';
+		private HashMap<String, Integer> wordMap;
+		private static int endouble=0;
+		
 		
 		public Text(String text) {
 			this.text=text;
+			wordMap= new HashMap<String, Integer>();
+			String newWord="";
 			for(int i=0;i<this.text.length();i++) {
-				if(text.charAt(i)==' ')textCache+=' ';
+				if(text.charAt(i)==' ') {
+					textCache+=' ';
+					if(!wordMap.containsKey(newWord.trim()))
+					wordMap.put(newWord.trim(),i-newWord.trim().length());
+					else {
+						wordMap.put(newWord.trim()+endouble, i-newWord.trim().length());
+						endouble++;
+					}
+					newWord="";
+				}
+				else if(text.charAt(i)=='.'||text.charAt(i)=='!'||text.charAt(i)=='?'||text.charAt(i)=='\''||text.charAt(i)==',') textCache+=text.charAt(i);
 				else textCache+=occultation;
+				newWord+=text.charAt(i);
+				
+			}
+			for(String keys : wordMap.keySet()) {
+				System.out.println("Valeur clé :"+keys+"|valeur mot :"+wordMap.get(keys)+"|valeur fin mot :" +(keys.length()-1+wordMap.get(keys).intValue()));
 			}
 		}
 
@@ -26,5 +48,32 @@ public class Text {
 
 		public String getTextCache() {
 			return textCache;
+		}
+		public void searchAndReplace(String response) {
+				for(String key : wordMap.keySet()) {
+					if(key.equals(response)) {
+						replace(key,response);
+						}
+					for(int i=0;i<endouble;i++) {
+						if(key.equals(response+i)) replace(key,response);
+					}
+			}
+			
+		}
+		
+		private void replace(String key,String response) {
+			char[] chars=textCache.toCharArray();
+			textCache="";
+			int z=0;
+			for(int i = wordMap.get(key);i<key.length()+wordMap.get(key).intValue();i++) {
+				if(z==response.length()) break;
+				chars[i]=response.charAt(z);
+				if(z<=response.length() && response.equals(key))z++;
+				if(z<=response.length() && !response.equals(key))z++;
+			}
+			for(int j=0;j<text.length();j++) {
+				System.out.println(chars[j]);
+				textCache+=chars[j];
+			}
 		}
 }
--- AppEtudiant/src/application/test.java
+++ AppEtudiant/src/application/test.java
@@ -1,11 +1,24 @@
 package application;
+import java.util.Scanner;
 
 public class test {
 
 	public static void main(String[] args) {
-		Text bit = new Text("Salut frérot t'es beau !");
+		String response;	
+		Scanner in = new Scanner(System.in);
+		Text bit = new Text("Salut frérot t'es beau frère ! Salut , tu es magnifique , Salut Salut Salut Salut Salut Salut Salut Salut ");
 		System.out.println(bit.getText());
 		System.out.println(bit.getTextCache());
+		while(!bit.getText().equals(bit.getTextCache())) {
+			System.out.print( "Entre ton mot\n" );
+			while(!in.hasNextLine()) {
+				in.next();
+			}
+			response = in.nextLine();
+			bit.searchAndReplace(response);
+			System.out.println(bit.getTextCache());
+		}
+		System.out.println("Bravo tu as terminé");
 	}
 
 }
